5e62b854f6872a30d5cdd60ea63c9e4b35c0e204
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-05 15:03:54 -0500

    1. My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from calculator_1 import *
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
