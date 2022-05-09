d4a6be35f85aeba3c1ca42ad75f8d7f098dd84f7
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-09 14:40:47 -0500

    2. Replace element 02

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
+    qtty = len(my_list)
 
-    my_list[idx] = element
+    if (0 <= idx < qtty):
+        my_list[idx] = element
     return my_list
