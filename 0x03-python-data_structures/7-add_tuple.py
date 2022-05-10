5406aefef7072d6b5105b8393a2fc2ba24467bf4
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-09 20:44:01 -0500

    7. Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    
+    if len(tuple_a) == 0:
+        ta = (0, 0)
+    elif len(tuple_a) == 1:
+        ta = (tuple_a[0], 0)
+    else:
+        ta = tuple_a
+    if len(tuple_b) == 0:
+        tb = (0, 0)
+    elif len(tuple_b) == 1:
+        tb = (tuple_b[0], 0)
+    else:
+        tb = tuple_b
+    tc = (ta[0] + tb[0], ta[1] + tb[1])
+    return tc
