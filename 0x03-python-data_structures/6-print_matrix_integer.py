a6aeff29e28bc3fad825b08426fd4ea58b8273e3
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-09 23:34:24 -0500

    6. Lists of lists = Matrix 09

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-This script is used to print a matrix of integers.
-
-Author: Ricardo Montaña
-Date: 9-05-2022
-"""
-
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
@@ -13,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print(matrix[row][col], end=" ")
             else:
                 print(matrix[row][col], end="")
-        print()
+        print("")
