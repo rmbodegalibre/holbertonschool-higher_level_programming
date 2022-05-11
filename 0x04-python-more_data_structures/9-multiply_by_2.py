70c4b0769dabe6a8da725363126491001bbc1e88
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-10 22:35:12 -0500

    9. Multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    b_dictionary = a_dictionary.copy()
+    for key, value in b_dictionary.items():
+        b_dictionary[key] = value * 2
+    return b_dictionary
