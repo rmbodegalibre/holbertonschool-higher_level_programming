2d04509eb18dbf3b42a7b9a4d7f25d7f6be92709
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-10 19:14:14 -0500

    0. Squared simple 02

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
     matrix_2 = []
-    
     for lst in matrix:
         new_lst = list([num**2 for num in lst])
         matrix_2.append(new_lst)
-    
     return matrix_2
