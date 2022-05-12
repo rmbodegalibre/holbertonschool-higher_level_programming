84399b367f8c3c3a7890ed394119fdf2f61c8c66
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-11 23:14:23 -0500

    2. Roman to Integer 03

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if roman_string:
+    if not roman_string or roman_string is None:
+        return 0
+    else:
         letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
         integer = 0
         for idx in range(len(roman_string)):
@@ -10,5 +12,3 @@ def roman_to_int(roman_string):
             else:
                 integer += letters[roman_string[idx]]
         return integer
-    else:
-        return 0
