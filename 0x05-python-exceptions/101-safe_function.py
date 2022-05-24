52b70a9e8142c2af4db25fa03bea908861f948e9
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-23 23:28:09 -0500

    8. Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        ptr_to_func = fct(*args)
+    except Exception as err:
+        ptr_to_func = None
+        print("Exception: {}".format(err), file=sys.stderr)
+    finally:
+        return ptr_to_func
