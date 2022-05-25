6f6327c07d9d0ffeb1e1c507c6d3629fa6d2c006
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-24 21:28:05 -0500

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Square: Define a class to build a square
+"""
+
+
+class Square:
+    """ Initializing an instance of class square  """
+    def __init__(self, size):
+        """ Defining size like private instance """
+        self.__size = size
