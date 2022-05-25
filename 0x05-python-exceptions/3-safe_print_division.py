eed115db1726e555107021afb2f1a5bec8d8d93a
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-24 19:11:53 -0500

    3-safe_print_division.py 12

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         res = a / b
-    except:
+    except (TypeError, ValueError):
         res = None
     finally:
         print("Inside result: {}".format(res))
