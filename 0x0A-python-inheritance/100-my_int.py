e70b7cd8d53f6a67ce01f6f794f45e42288e2f02
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-08 00:36:45 -0500

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+File: 100-my_int.py - Write a class MyInt that inherits from int
+"""
 
 
+class MyInt(int):
+    """
+    MyInt class is defined below
+    """
+    def __eq__(self, other):
+        """
+        Return: True if equal
+        """
+        return self - other != 0
 
+    def __ne__(self, other):
+        """
+        Return: True if not equal
+        """
+        return self - other == 0
