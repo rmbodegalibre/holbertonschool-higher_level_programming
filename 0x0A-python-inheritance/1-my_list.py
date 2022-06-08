4783f05bb00196ea7dc0bde557bbff4705594eae
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-07 20:23:03 -0500

    1. My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+lookup: Return a list of available attributes and methods of the object
+"""
 
 
-
+class MyList(list):
+    """
+    Method print_sorted prints the list in order
+    """
+    def print_sorted(self):
+        print(sorted(self))
