0a5aaf593957dc24ae9f0b6ee30ca4197d8a9581
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-08 01:06:30 -0500

    101-add_attribute.py 02

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,12 +9,11 @@ def add_attribute(obj, a, v):
     """
     add_attribute function is defined below
     """
-    res = getattr(obj, "__doc__", None)
-    if res is None:
-        setattr(obj, a, v)
-        """ setattr adds new attribute """
-    else:
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
         """
-        If this isn't possible, raise an error
+        If this has attribute, raise an error
         """
+    else:
+        setattr(obj, a, v)
+        """ setattr adds new attribute """
