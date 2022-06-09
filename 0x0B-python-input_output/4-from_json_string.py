3aa8fae5f420b5735c1f59b7e930c206a41c82ae
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-08 21:38:53 -0500

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+File: 4-from_json_string.py - Write a function that returns an object
+(Python data structure) represented by a JSON string:
+"""
+
+
+def from_json_string(my_str):
+    """
+    from_json_string function Write a function that returns an object
+    """
+    return json.loads(my_str)
