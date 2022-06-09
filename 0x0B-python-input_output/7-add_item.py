c631f410ba7fbd77f73110e5abdf2ca719ad5eef
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-08 22:11:53 -0500

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+File: 7-add_item.py - Write a script that adds all arguments to a Python list,
+and then save them to a file
+"""
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+if os.path.isfile(filename):
+    """
+    os.path.is - check if the path is an existing file
+    """
+    file_o = load_from_json_file(filename)
+else:
+    file_o = []
+file_o.extend(sys.argv[1:])
+"""
+file_o.extend - get extension form file_object
+"""
+save_to_json_file(file_o, filename)
