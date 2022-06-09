28648bf270c0915ff184e4296fa2737727b35d88
rmbodegalibre <rmbodegalibre@gmail.com>
2022-06-08 21:15:28 -0500

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+File: 0-read_file.py - Write a function that reads a text file (UTF8)
+and prints it to stdout:
+"""
+
+
+def read_file(filename=""):
+    """
+    read_file function is defined below
+    """
+    with open(filename, encoding="utf-8") as f:
+        """
+        Method open returns returns a file object
+        """
+        print(f.read(), end="")
