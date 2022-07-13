5356d0593f979c7a8dc333b9847f5abf8fc8af4f
rmbodegalibre <rmbodegalibre@gmail.com>
2022-07-13 11:33:01 -0500

    0-list_databases.sql03

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,6 +1,5 @@
---This file is used for display a list of database
-Database                                                                                   
-information_schema                                                                         
-mysql                                                                                      
-performance_schema                                                                         
-sys
+--This file is used for display a list of databa
+show databases;                           
+                                                                  
+                                                                    
+
