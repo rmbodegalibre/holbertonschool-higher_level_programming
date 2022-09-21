2a1582aad99b2fecb059f5a9e061db53b2e1c18d
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-21 14:21:42 -0500

    7-islower.py

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    return True if 96 < ord(c) < 123 else False
+
+"""
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    else:
+        return False
+"""
