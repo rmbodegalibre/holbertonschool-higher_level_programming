52066749dbb298343095bb03c19e2eb62a116e42
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-23 14:26:58 -0500

    9-max_integer.py 03

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    my_list.sort(reverse=True)
-    return my_list[0]
+    if my_list:
+        if len(my_list) == 0:
+            return "None"
+        else:
+            my_list.sort(reverse=True)
+            return my_list[0]
+ 
