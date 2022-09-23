f20ff880a38036792396127b958569c1b1e59570
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-22 23:39:12 -0500

    1-calculation.py 03

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import *
 if __name__ == "__main__":
-    print("10 + 5 = {}".format(add(10,5)))
-    print("10 - 5 = {}".format(sub(10,5)))
-    print("10 * 5 = {}".format(mul(10,5)))
-    print("10 / 5 = {}".format(div(10,5)))
+    a = 10
+    b = 5
+    print("10 + 5 = {}".format(add(a,b)))
+    print("10 - 5 = {}".format(sub(a,b)))
+    print("10 * 5 = {}".format(mul(a,b)))
+    print("10 / 5 = {}".format(div(a,b)))
