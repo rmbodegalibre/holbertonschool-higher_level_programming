614fd7aee5e5995f92b22b196dbda13c61750aef
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-23 00:49:21 -0500

    2-args.py 03

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 import sys
 
-py_args = sys.argv
-py_args.pop(0)
-py_len = len(py_args)
-j = 0
+if __name__ == "__main__":
+    py_args = sys.argv
+    py_args.pop(0)
+    py_len = len(py_args)
+    j = 0
 
-if py_len == 1:
-    print("1 argument:")
-elif py_len == 0:
-    print("{} arguments.".format(py_len))
-else:
-    print("{} arguments:".format(py_len))
+    if py_len == 1:
+      print("1 argument:")
+    elif py_len == 0:
+        print("{} arguments.".format(py_len))
+    else:
+        print("{} arguments:".format(py_len))
 
-for i in py_args:
-    j += 1
-    print("{}: {}".format(j, i))
+    for i in py_args:
+        j += 1
+        print("{}: {}".format(j, i))
