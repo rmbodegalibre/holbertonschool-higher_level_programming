25423a4a46e38df68dc9b9d506c3c3ca03ebe84d
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-23 14:08:40 -0500

    8-multiple_returns.py 02

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     lenght = len(sentence)
-    first_c = sentence[0]
+    first_c = "None" if lenght == 0 else sentence[0]
     return lenght, first_c
