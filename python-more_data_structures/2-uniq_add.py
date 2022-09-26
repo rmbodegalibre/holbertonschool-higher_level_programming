51cbc3bb74edb14e12bd90f4c7b85659103cdad4
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-26 07:46:20 -0500

    2-uniq_add.py 05

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,8 @@ def uniq_add(my_list=[]):
         if len(my_list) == 0:
             return 0
         else:
-            new_list = set(list(filter(lambda num:str(num).isdigit(), my_list)))
+            # new_list = set(list(filter(lambda num:str(num).isdigit(), my_list)))
+            new_list = set(list(filter(lambda num:num, my_list)))
+            # print(new_list)
             return sum(new_list)
     return 0
