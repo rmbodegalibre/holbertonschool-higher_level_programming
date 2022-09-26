f9a599af43821f9f17357ad438de7fa3eaa66523
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-26 01:25:04 -0500

    1-search_replace.py 0

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    ind = my_list.index(search)
-    new_list = list(my_list)
-    new_list[ind] = replace
-    return new_list
+    if search not in my_list:
+        return my_list
+    else:
+        ind = my_list.index(search)
+        new_list = list(my_list)
+        new_list[ind] = replace
+        return new_list
