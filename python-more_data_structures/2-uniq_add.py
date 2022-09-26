00c2bce088b18c9f5fd88b8729c656bebe35d250
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-26 01:42:40 -0500

    2-uniq_add.py 02

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     if my_list:
-        new_list = list(set(my_list))
-        result = 0
-        for i in new_list:
-            result += i
-        return result
+        if len(my_list) == 0:
+            return 0
+        else:
+            new_list = list(set(my_list))
+            result = 0
+            for i in new_list:
+                result += i
+            return result
