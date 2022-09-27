e1587164eec77d4dee73093b5232eca88bda8ee7
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-26 19:11:19 -0500

    11-multiply_list_map.py 02

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-
-    def multiplyBy(num):
-        #return num * number
-        return num * number
-
-    multipliedNumbers = list(map(multiplyBy, my_list))
-    return multipliedNumbers
+    return list(map(lambda num:num * number, my_list))
