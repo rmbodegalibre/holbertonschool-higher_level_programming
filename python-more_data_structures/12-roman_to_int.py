5b7604fba720efd2706fd14f7b945f76196b5f42
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-26 21:13:26 -0500

    12-roman_to_int.py 02

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or roman_string is None):
+    if not roman_string:
         return (0)
     else:
-        nr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':1000}
+        nr = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         itg = 0
         ant = 0
         for i in roman_string:
