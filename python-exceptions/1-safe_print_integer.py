ab0125c741198468b9eaaa9c5bc89694989732a9
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-27 22:44:05 -0500

    1-safe_print_integer.py 02

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError or TypeError:
+    except ValueError:
+        return False
+    else:
         return False
