34a6c52cee6f0dcf9be6d73a79d4823fd7928b82
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-27 21:14:59 -0500

    0-safe_print_list.py

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        if my_list and x > 0:
+
+            pos = 0
+            str_dev = ""
+            for i in my_list:
+                str_dev += str(i)
+                pos += 1
+                if pos == x:
+                    break
+    except TypeError:
+        return pos
+    else:
+        print(str_dev)
+        return pos
