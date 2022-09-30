ce2b235d4a8142f331caea63f1850270b58135b4
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-30 09:30:05 -0500

    2-square.py 02

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This is class that define a square
 """
@@ -10,11 +9,11 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-    if isinstance(size, int):
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                """ Defining private __size """
+                self.__size = size
         else:
-            """ Defining size like private instance """
-            self.__size = size
-    else:
-        raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
