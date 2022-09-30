9e3581fc5d9da053626b1ce876ab192383c1e609
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-29 23:22:23 -0500

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This is class that define a square
+"""
+
+
+class Square:
+    """Pass is used to declare an empty function"""
+    def __init__(self, size):
+        self.__size = size
