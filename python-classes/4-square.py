ae478816817d48ae887ec868faacccbc10f660ea
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-30 17:27:55 -0500

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Square: Define a class to build a square
+"""
+
+
+class Square:
+    """ Initializing an instance of class square  """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """ Return the size of the square """
+        return (self.__size)
+
+    @size.setter
+    def size(self, val):
+        """ Property by which the size is defined """
+        if not isinstance(val, int):
+            raise TypeError("size must be an integer")
+        elif val < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = val
+
+    def area(self):
+        """ Returns the result of squaring size. """
+        return (self.__size ** 2)
