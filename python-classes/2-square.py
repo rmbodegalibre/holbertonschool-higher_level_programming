6038d0029f707f48518b3dc38f2b3f724f5dc710
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-30 09:21:27 -0500

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This is class that define a square
+"""
+
+
+class Square:
+    """Define constructor to init the class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    if isinstance(size, int):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """ Defining size like private instance """
+            self.__size = size
+    else:
+        raise TypeError("size must be an integer")
