25f928d8c5a99e918cdfbc16a337b1df6bf4d119
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-05 01:44:16 -0500

    5-text_indentation.py & 5-text_indentation.txt

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+5-text_indentacion.py
+This program prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    This function prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    text must be a string,
+    otherwise raise a TypeError exception with the message:
+    "text must be a string"
+    There should be no space at the beginning
+    or at the end of each printed line
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+        return
+
+    pos = -1
+    for i in text:
+        pos += 1
+        if i == " " and pos == 0:
+            continue
+        elif i == " " and text[pos-1] in [" ", ".", "?", ":"]:
+            continue
+        elif i in [".", "?", ":"]:
+            print(i)
+            print()
+        else:
+            print(i, end="")
