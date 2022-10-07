8281a76aef1b67ff72162693706c15da9a6952d4
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-06 21:49:37 -0500

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This program contains an empty class Rectangle that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ This is an empty class that defines a rectangle"""
+    pass
