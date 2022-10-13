3cfb896d7eaf89527415d5e3d29bbed4b5a2f3f2
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-13 17:13:07 -0500

    0-lookup.py

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This program contains a function that returns the list of
+available attributes and methods of an object:
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the list of available attributes
+    and methods of an object:
+    """
+    return obj.__dict__
