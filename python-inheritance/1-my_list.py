48f19a24c0e52e0a24501f60ee7b2c3b0143db87
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-14 08:31:28 -0500

    1-my_list.py & 1-my_list.txt 02

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ This program contains a class MyList that inherits from list:
 
 
 class MyList(list):
-    """It's a subclass from list class"""
+    """It's a subclass of list class"""
     def print_sorted(self):
-        """This function prints a list yn sorted order"""
+        """This function prints a list in sorted order"""
         print(sorted(self))
