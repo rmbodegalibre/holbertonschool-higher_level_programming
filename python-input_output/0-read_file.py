7d8b5a3656cc819774d411af8ab7189e956790dc
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-16 14:15:50 -0500

    0-read_file.py 03

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This file contains a function that reads a text file (UTF8) and prints it to stdout
+This file contains a function that reads a text file (UTF8) and prints it
+to stdout
 """
 
 
@@ -8,6 +9,6 @@ def read_file(filename=""):
     """
     This function reads a text file (UTF8) and prints it to stdout
     """
-    with open(filename, encoding = "utf-8") as fName:
+    with open(filename, encoding="utf-8") as fName:
         read_data = fName.read()
     print(read_data, end="")
