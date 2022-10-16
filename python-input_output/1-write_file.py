53865269be0341a5a6989a17c38ee30482bab3c8
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-16 15:10:06 -0500

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """
-
+This module contains a function that writes a string to a text file (UTF8)
+and returns the number of characters written
 """
 
+
 def write_file(filename="", text=""):
-    """"""
+    """
+    This function that writes a string to a text file (UTF8)
+    and returns the number of characters written:
+    * It uses the with statement
+    * It should create the file if doesn’t exist.
+    * should overwrite the content of the file if it already exists.
+    """
+    with open(filename, "r+", encoding="utf-8") as fName:
+        fName.write(text)
+    return len(text)
