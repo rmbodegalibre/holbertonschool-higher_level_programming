3ce34cdfb2e6a00473ce6c6272c81c8e6161345f
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-16 22:56:09 -0500

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """
-
+File: 8-class_to_json.py - Write a function that returns the dictionary
+description with simple data structure (list, dictionary, string, integer
+and boolean) for JSON serialization of an object
 """
 
+
 def class_to_json(obj):
-    """"""
+    """
+    class_to_json(obj) - returns the dictionary description with simple
+    data structure
+    """
+    return obj.__dict__
