6022c86d343812fa21cbfac4f4fa0b4e452bd468
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-16 23:00:04 -0500

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python3
 """
-
+File: 10-student.py - Write a class Student that defines a student by:
+(based on 9-student.py)
 """
 
-class Student():
-    """"""
+
+class Student:
+    """
+    class that represents a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        to_json - retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dictio = dict()
+            for idx in attrs:
+                if idx in self.__dict__:
+                    dictio[idx] = self.__dict__[idx]
+            return dictio
