03645b3abcb1088574514dd45b88955c6c2c212a
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-16 23:13:46 -0500

    12-pascal_triangle.py 02

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """
 File: 12-pascal_triangle.py - Create a function def pascal_triangle(n): that
 returns a list of lists of integers representing the Pascal’s triangle of n
@@ -13,3 +12,13 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
+    p_t = [[1]]
+    while len(p_t) != n:
+        tri = p_t[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        p_t.append(tmp)
+    return p_t
