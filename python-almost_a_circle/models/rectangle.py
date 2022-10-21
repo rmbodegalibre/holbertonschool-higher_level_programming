6dc384587eada031faa5518029eed333df87f65c
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-21 15:47:30 -0500

    rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains the class Rectangle that inherits from Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+
