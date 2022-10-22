3a027d4b80b019bcf15afbe4f9e8b3153b191f63
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-22 17:19:54 -0500

    models/rectangle.py 14

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -139,6 +139,6 @@ class Rectangle(Base):
         if len(args) > 2:
             self.__height = args[2]
         if len(args) > 3:
-            self.__x = args [3]
+            self.__x = args[3]
         if len(args) > 4:
-            self.__y = args [4]
+            self.__y = args[4]
