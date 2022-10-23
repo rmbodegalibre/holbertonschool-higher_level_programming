8b6f04ffa38ed14ac8866aa1c196818ec25aff79
rmbodegalibre <rmbodegalibre@gmail.com>
2022-10-23 00:00:27 -0500

    models/square.py 03

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,3 +25,14 @@ class Square(Rectangle):
         """
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.height
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
