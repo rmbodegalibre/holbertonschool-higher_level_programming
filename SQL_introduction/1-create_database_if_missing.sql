767c241c7eb2f73b7175af5270969567d3f6dc22
rmbodegalibre <rmbodegalibre@gmail.com>
2022-11-11 16:59:44 -0500

    1-create_database_if_missing.sql 04

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
---This script creates the database hbtn_0c_0 in MySQL server
-DROP DATABASE IF EXISTS hbtn_0c_0;
-CREATE DATABASE hbtn_0c_0;
+-- This script creates the database hbtn_0c_0 in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
