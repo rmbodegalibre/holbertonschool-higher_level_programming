38abc0a2264529e7dfd110625b8cf123728e7b77
rmbodegalibre <rmbodegalibre@gmail.com>
2022-11-11 21:52:56 -0500

    16-no_link.sql

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- This script lists all records of the table second_table
+-- of the database hbtn_0c_0 in MySQL server.
+SELECT score, name FROM second_table WHERE name IS NOT null ORDER BY score DESC;
