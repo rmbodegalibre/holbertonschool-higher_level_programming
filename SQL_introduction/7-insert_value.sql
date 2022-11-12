cf29410206e3f8c3126c541f170007fafa61ec09
rmbodegalibre <rmbodegalibre@gmail.com>
2022-11-11 19:52:53 -0500

    7-insert_value.sql 03

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- This script inserts a new row in the table
 -- first_table (database hbtn_0c_0) in MySQL server.
-INSERT INTO fist_table (id, name) VALUES(89, "Best School");
+INSERT INTO fist_table (id, name) VALUES(89, Best School);
