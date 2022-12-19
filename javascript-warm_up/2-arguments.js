f9f3462bb2f4498a2a5e55a1083cbe7edfaaf9f2
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-19 16:24:45 -0500

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // print process.argv
-let num_args = process.argv.length;
-console.log(num_args === 2 ? 'No argument' : num_args === 3 ? 'Argument found' : 'Arguments found');
+let numArgs = process.argv.length;
+console.log(numArgs === 2 ? 'No argument' : numArgs === 3 ? 'Argument found' : 'Arguments found');
