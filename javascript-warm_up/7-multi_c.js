18ba9dcdddebbe96500117354dd013040a946e67
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-20 06:34:10 -0500

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 // This script prints x times “C is fun”
 const myVar = Math.floor(Number(process.argv[2]));
 if (!isNaN(myVar)) {
-    for (let i = 0; i < myVar; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < myVar; i++) {
+    console.log('C is fun');
+  }
 } else {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
