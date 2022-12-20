1f1bc73a7cd113bb8f4bea509fc1cc2a1e92618e
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-20 06:56:11 -0500

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 // The first argument is the first integer
 // The second argument is the second integer
 // It defines a function with this prototype: function add(a, b)
-function sum(a, b) {
+function sum (a, b) {
   return a + b;
 }
 
