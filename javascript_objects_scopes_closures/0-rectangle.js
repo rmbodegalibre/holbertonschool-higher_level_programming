b30677d46e9cc298dcbcf9687169ec6fe2d74a46
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-20 12:35:45 -0500

    0-rectangle.js

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    // define the properties
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+};
