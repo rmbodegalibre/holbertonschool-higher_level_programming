6cc533a912b945f1851d8961247dd62f8f41916c
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-19 22:37:03 -0500

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,5 +3,5 @@
 // but by using an array of string and a loop
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myVar.length; i++) {
-    console.log(myVar[i]);
+  console.log(myVar[i]);
 }
