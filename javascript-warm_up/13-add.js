67a42a42470a6de27fbadadec2ef812319871be2
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-20 07:42:12 -0500

    13-add.js

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// This script contains a function that returns the addition of 2 integers.
+// The function is visible from outside
+// The name of the function is add
+exports.add = (a, b) => a + b;
