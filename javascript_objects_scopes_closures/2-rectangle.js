d8ee800bc9b7202e4204af2bc97a7d28d4fb5385
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-21 01:00:53 -0500

    2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -6,10 +6,10 @@
 // This initializes the instance attribute height with the value of h
 // If w or h is equal to 0 or not a positive integer, create an empty object
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 };
