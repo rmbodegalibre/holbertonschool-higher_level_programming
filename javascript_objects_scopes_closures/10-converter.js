837481aa6b908787c723abe863ecca0dc910e751
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-21 08:11:18 -0500

    10-converter.js

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script contains a function that converts a number from base 10 to another base passed as argument:
+// No files are imported in this script
+// No new variebles are declared in this script (var, let, etc)
+exports.converter = function (base) {
+  return converted => converted.toString(base);
+};
