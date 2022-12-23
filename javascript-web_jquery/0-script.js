03c057b4b1cc55c2f7276cf2268619d82bd595c6
rmbodegalibre <rmbodegalibre@gmail.com>
2022-12-23 11:05:14 -0500

    0-script.js

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// This script updates the text color of the <header> element to red (#FF0000):
+// It uses document.querySelector to select the HTML tag
+// It doesn't the JQuery API
+document.querySelector('HEADER').style.color = '#FF0000';
